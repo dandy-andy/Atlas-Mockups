a9c81660e0bb300536c3185fc9ed0736c2d24da7
dandy-andy <andrewpkennedy@gmail.com>
2022-07-09 07:36:09 +1000

    3 files modified

--- src/app/page-layout/page-layout.component.ts
+++ src/app/page-layout/page-layout.component.ts
@@ -10,7 +10,85 @@ export class PageLayoutComponent {
 
   public readonly layoutWidthOptions: number[] = [1, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
 
-  public readonly pages = [];
+  public readonly pages = [
+    {
+      sec: "A",
+      no: 1,
+      w: 240,
+      h: 380,
+      shapes: [
+        { t: 2, x: 0, y: 0, w: 240, h: 285 },
+        { t: 1, x: 0, y: 285, w: 240, h: 95 },
+      ],
+    },
+    { sec: "A", no: 2, w: 240, h: 380, shapes: [{ t: 1, x: 0, y: 285, w: 240, h: 95 }] },
+    { sec: "A", no: 3, w: 240, h: 380, shapes: [{ t: 1, x: 120, y: 190, w: 120, h: 190 }] },
+    { sec: "A", no: 4, w: 240, h: 380, shapes: [{ t: 1, x: 0, y: 190, w: 120, h: 190 }] },
+    { sec: "A", no: 5, w: 240, h: 380, shapes: [{ t: 1, x: 120, y: 190, w: 120, h: 190 }] },
+    { sec: "A", no: 6, w: 240, h: 380, shapes: [{ t: 1, x: 0, y: 190, w: 120, h: 190 }] },
+    { sec: "A", no: 7, w: 240, h: 380, shapes: [{ t: 1, x: 120, y: 190, w: 120, h: 190 }] },
+    { sec: "A", no: 8, w: 240, h: 380, shapes: [{ t: 1, x: 0, y: 190, w: 120, h: 190 }] },
+    { sec: "A", no: 9, w: 240, h: 380, shapes: [{ t: 1, x: 120, y: 190, w: 120, h: 190 }] },
+    { sec: "A", no: 10, w: 240, h: 380, shapes: [{ t: 1, x: 0, y: 0, w: 240, h: 380 }] },
+    { sec: "A", no: 11, w: 240, h: 380, shapes: [{ t: 1, x: 0, y: 0, w: 240, h: 380 }] },
+    {
+      sec: "A",
+      no: 12,
+      w: 240,
+      h: 380,
+      shapes: [
+        { t: 1, x: 0, y: 190, w: 240, h: 190 },
+        { t: 1, x: 0, y: 0, w: 120, h: 190 },
+      ],
+    },
+    { sec: "A", no: 13, w: 240, h: 380, shapes: [{ t: 1, x: 120, y: 190, w: 120, h: 190 }] },
+    { sec: "A", no: 14, w: 240, h: 380, shapes: [{ t: 1, x: 0, y: 190, w: 120, h: 190 }] },
+    {
+      sec: "A",
+      no: 15,
+      w: 240,
+      h: 380,
+      shapes: [
+        { t: 1, x: 120, y: 190, w: 120, h: 190 },
+        { t: 1, x: 0, y: 190, w: 120, h: 190 },
+      ],
+    },
+    { sec: "A", no: 16, w: 240, h: 380, shapes: [] },
+    { sec: "A", no: 17, w: 240, h: 380, shapes: [{ t: 1, x: 120, y: 190, w: 120, h: 190 }] },
+    { sec: "A", no: 18, w: 240, h: 380, shapes: [] },
+    { sec: "A", no: 19, w: 240, h: 380, shapes: [{ t: 1, x: 120, y: 190, w: 120, h: 190 }] },
+    { sec: "A", no: 20, w: 240, h: 380, shapes: [] },
+    { sec: "A", no: 21, w: 240, h: 380, shapes: [{ t: 1, x: 120, y: 190, w: 120, h: 190 }] },
+    {
+      sec: "A",
+      no: 22,
+      w: 240,
+      h: 380,
+      shapes: [
+        { t: 1, x: 0, y: 0, w: 240, h: 190 },
+        { t: 1, x: 0, y: 190, w: 120, h: 190 },
+      ],
+    },
+    { sec: "A", no: 23, w: 240, h: 380, shapes: [{ t: 1, x: 120, y: 190, w: 120, h: 190 }] },
+    { sec: "A", no: 24, w: 240, h: 380, shapes: [] },
+    {
+      sec: "A",
+      no: 25,
+      w: 240,
+      h: 380,
+      shapes: [
+        { t: 1, x: 0, y: 190, w: 240, h: 190 },
+        { t: 1, x: 0, y: 0, w: 240, h: 190 },
+      ],
+    },
+    { sec: "A", no: 26, w: 240, h: 380, shapes: [{ t: 1, x: 0, y: 285, w: 240, h: 95 }] },
+    { sec: "A", no: 27, w: 240, h: 380, shapes: [] },
+    { sec: "A", no: 28, w: 240, h: 380, shapes: [{ t: 1, x: 0, y: 285, w: 240, h: 95 }] },
+    { sec: "A", no: 29, w: 240, h: 380, shapes: [] },
+    { sec: "A", no: 30, w: 240, h: 380, shapes: [{ t: 1, x: 0, y: 0, w: 240, h: 380 }] },
+    { sec: "A", no: 31, w: 240, h: 380, shapes: [{ t: 2, x: 0, y: 0, w: 240, h: 380 }] },
+    { sec: "A", no: 32, w: 240, h: 380, shapes: [{ t: 2, x: 0, y: 0, w: 240, h: 380 }] },
+  ];
 
   public get spreads(): any[] {
     let spreads: [any, any][] = [[null, this.pages[0]]];
@@ -22,11 +100,5 @@ export class PageLayoutComponent {
 
   constructor() {
     this.currentLayoutWidthPages = 8;
-
-    let initPages = [];
-    for (let i = 1; i <= 32; i++) {
-      initPages.push({ pageNo: i, shapes: Math.floor(Math.random() * 8) });
-    }
-    this.pages = initPages;
   }
 }
