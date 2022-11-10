7712aaf2f1a1a8091a997604d9e9371e0d32cfb1
dandy-andy <86581816+dandy-andy@users.noreply.github.com>
2022-11-10 17:39:12 +1100

    3 files modified

--- src/app/format-page-properties-pane/format-page-properties-pane.component.ts
+++ src/app/format-page-properties-pane/format-page-properties-pane.component.ts
@@ -1,12 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-format-page-properties-pane',
-  templateUrl: './format-page-properties-pane.component.html',
-  styleUrls: ['./format-page-properties-pane.component.css'],
+  selector: "app-format-page-properties-pane",
+  templateUrl: "./format-page-properties-pane.component.html",
+  styleUrls: ["./format-page-properties-pane.component.css"],
 })
 export class FormatPagePropertiesPaneComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  readonly allItems = [...Array(100).keys()];
+
+  selectedItems = [];
+
+  get items() {
+    return this.allItems.filter((x) => (this.showOdd && x % 2 !== 0) || (this.showEven && x % 2 === 0));
+  }
+
+  showOdd = true;
+
+  get showEven() {
+    return !this.showOdd;
+  }
+  set showEven(val) {
+    this.showOdd = !this.showOdd;
+  }
 }
